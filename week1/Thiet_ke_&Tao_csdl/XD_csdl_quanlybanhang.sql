3fdb8497c48291064f98a27e50f24c2e4022ef28
Sinh Cute <baosinh2k@gmail.com>
2022-07-01 10:03:48 +0700

    update_qlybanhang

---
+++ week1/Thiet_ke_&Tao_csdl/XD_csdl_quanlybanhang.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE QUANLIBANHANG;
+USE quanlibanhang;
+CREATE TABLE Customer (
+    cID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cName VARCHAR(50) NOT NULL,
+    cAge INT
+);
+CREATE TABLE Orders (
+    oID INT NOT NULL PRIMARY KEY,
+    oDate DATE NOT NULL,
+    oTotalPrice DOUBLE,
+    cID INT,
+    FOREIGN KEY (cID)
+        REFERENCES Customer (cID)
+);
+CREATE TABLE Product (
+    pID INT NOT NULL PRIMARY KEY,
+    pName VARCHAR(50),
+    pPrice DOUBLE
+);
+CREATE TABLE OrderDetail (
+    oID INT NOT NULL,
+    pID INT NOT NULL,
+    odQTY VARCHAR(50),
+    FOREIGN KEY (oID) REFERENCES Orders (oID),
+    FOREIGN KEY (pID) REFERENCES Product (pID)_x
+);
